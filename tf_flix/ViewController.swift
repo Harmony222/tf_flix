9493c8f58985b6fb61e96ed3c4f8d4d2c98dac35
harmony222 <scarleth@oregonstate.edu>
2021-07-07 09:38:09 -0700

    Initial Commit

---
+++ tf_flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tf_flix
+//
+//  Created by Harmony Scarlet on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
